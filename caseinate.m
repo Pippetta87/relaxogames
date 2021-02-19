222c0007883440e9890c88feaaa78d6a5979d31c
salvicio@horsepower <emserfo@gmail.com>
2021-02-19 22:31:00 +0100

    first commit where started fitting some lorentzian

---
+++ caseinate.m
@@ -0,0 +1,26 @@
+## Copyright (C) 2021 Utente
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn {} {@var{retval} =} caseinate (@var{input1}, @var{input2})
+##
+## @seealso{}
+## @end deftypefn
+
+## Author: Utente <Utente@ASUS>
+## Created: 2021-02-13
+
+omegaexp=[1.1*10^5 1.7*10^5 2.5*10^5 4.7*10^5 6*10^5 8*10^5 9*10^5 1.2*10^6 1.3*10^6 1.7*10^6 1.9*10^6 2.1*10^6 2.6*10^6 3*10^6 3.2*10^6 3.5*10^6 3.9*10^6 5.5*10^6 7.5*10^6 1.2*10^7 1.9*10^7 3.2*10^7 5.7*10^7 8.5*10^7];
+rexp=[4.47 4.45 4.40 4.38 4.16 4.1 3.8 3.6 3.4 3.1 2.8 2.5 2.2 1.9 1.7 1.4 1.2 0.8 0.52 0.32 0.18 0.13 0.10 0.08];
